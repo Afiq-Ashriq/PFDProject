1a8dd3a15ea9c062741a9a92a8d83629c04714d6
JesiahChow <S10221066@connect.np.edu.sg>
2023-11-06 19:50:35 +0800

    fixed styling and formatting for login

--- login.js
+++ login.js
@@ -1,42 +1,39 @@
-const input = document.querySelectorAll("input");
-button = document.querySelector("button");
 
 
-//iterate over all inputs
-input.forEach((index,index1)=> {
-    index.addEventListener("keyup",(e)=>{
-        const currentInput = index,
-        nextInput = index.nextElementSibling,
-        prevInput = index.previousElementSibling;
-        //if value has more than 1 character then clear it
-        if(currentInput.value.length > 1){
-            currentInput.value="";
-            return;
-        }
-        //if next input is disabled and the current value is not empty
-        //enable the next input
-        if(nextInput && nextInput.hasAttribute("disabled")&&currentInput.value !== ""){
-            nextInput.removeAttribute("disabled");
-            nextInput.focus();
-        }
-        //if backspace is pressed
-        if(e.key == "Backspace"){
-            input.forEach((inputs,index2)=> {
-                if(index1 <= index2 && prevInput){
-                    inputs.setAttribute("disabled",true)
-                    currentInput.value="";
-                    prevInput.focus();
-                }
-            })
-        }
-        if (document.querySelectorAll('.input-field input:not([disabled])').length == 6) {
-            button.removeAttribute('disabled');
-            button.addEventListener("click", function(){
-                window.location.href = "home.html";
-            })
-        } else {
-            enterButton.setAttribute('disabled', true);
-        }
-    })
-})
+const input = document.querySelector("input");
+const button = document.querySelector("button");
 
+input.addEventListener("keyup", (e) => {
+    const currentInput = e.target;
+    const nextInput = currentInput.nextElementSibling;
+    const prevInput = currentInput.previousElementSibling;
+
+    // Validate if the input is a number
+    if (isNaN(currentInput.value)) {
+        currentInput.value = "";
+        return;
+    }
+
+    // Enable the button if the input is not empty
+    if (currentInput.value !== "") {
+        button.setAttribute("disabled", true);
+    }
+
+    // Handle backspace
+    if (e.key == "Backspace" && prevInput) {
+        prevInput.removeAttribute("disabled");
+        prevInput.focus();
+    }
+    if (currentInput.value.length == 6) {
+        button.removeAttribute('disabled');
+        button.addEventListener("click", function(){
+            window.location.href = "home.html";
+        })
+    } else {
+        enterButton.setAttribute('disabled', true);
+    }
+});
+
+button.addEventListener("click", function () {
+    window.location.href = "index.html";
+});
